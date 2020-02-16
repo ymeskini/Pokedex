6984d725ffc57a8a0260459f9e253c8334cfd74a
Youssef Meskini <youss.meskini@gmail.com>
2020-02-16 18:33:29 +0100

    persisting data

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+
+import {persistStore} from 'redux-persist';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Text, View} from 'react-native';
@@ -12,31 +15,34 @@ import CreateCat from './src/screens/CreateCat';
 
 const Stack = createStackNavigator();
 const store = setupStore();
+const persistor = persistStore(store);
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            options={({navigation}) => ({
-              headerRight: () => (
-                <View>
-                  <Button onPress={() => navigation.navigate('CreateCat')}>
-                    <Text>New</Text>
-                  </Button>
-                </View>
-              ),
-            })}
-            name="Cats"
-            component={CatList}
-          />
-          <Stack.Screen name="ViewCat" component={ViewCat} />
-          <Stack.Screen name="CreateCat">
-            {props => <CreateCat {...props} dispatch={store.dispatch} />}
-          </Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              options={({navigation}) => ({
+                headerRight: () => (
+                  <View>
+                    <Button onPress={() => navigation.navigate('CreateCat')}>
+                      <Text>New</Text>
+                    </Button>
+                  </View>
+                ),
+              })}
+              name="Cats"
+              component={CatList}
+            />
+            <Stack.Screen name="ViewCat" component={ViewCat} />
+            <Stack.Screen name="CreateCat">
+              {props => <CreateCat {...props} dispatch={store.dispatch} />}
+            </Stack.Screen>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,37 @@
-import {configureStore, combineReducers} from '@reduxjs/toolkit';
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-community/async-storage';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
 import {catReducer} from '../slices/cats';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
 
 export const rootReducer = combineReducers({cats: catReducer});
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const setupStore = () =>
   configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
+    // https://github.com/rt2zz/redux-persist/issues/988#issuecomment-552242978
+    middleware: getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   });
